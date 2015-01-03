674cae1b8192946d601dc4bb0586e53d94bad51b
ingrid <isolovan@gmail.com>
2015-01-03 02:38:27 +0200

    Register_user Update

--- app/models/registered_user_for_publication.rb
+++ app/models/registered_user_for_publication.rb
@@ -3,7 +3,7 @@ class RegisteredUserForPublication < ActiveRecord::Base
   has_one 	:publication
   
   validates :publication_id, presence: true  
-  validates :date_of_registration, presence: true
+  #validates :date_of_registration, presence: true
   validates :active_device_dev_uuid, presence: true, length: { maximum: 64 }  
 
   before_save :init_data
