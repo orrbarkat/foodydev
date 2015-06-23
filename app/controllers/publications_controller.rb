5a48eb9a7513682725069272b0a833a5183d84b2
orrbarkat <barkat.orr@gmail.com>
2015-06-23 13:48:27 +0300

    Update publications_controller.rb

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -46,12 +46,12 @@ private
     connection.open
     #@devices.each do |device|
     notification = Houston::Notification.new(device:'fd01e0baab71ad02ffd4eb10e34daa06fbdb3352ce7286a20ef1333465bc494b')  #device.remote_notification_token) 
-    notification.alert = “New Publication around you #{publication.title}“
+    notification.alert = "New Publication around you" 
     notification.badge = 1
     notification.sound = ""
     notification.category = “ARRIVED_CATEGORY“
     notification.content_available = false
-    notification.custom_data = {’id’: publication.id, ’version’: publication.version ,’title’: publication.title}
+    notification.custom_data = {type:"new_publication",data:{ id: publication.id ,version: publication.version ,title: publication.title}}
     connection.write(notification.message)
     connection.close
   end
