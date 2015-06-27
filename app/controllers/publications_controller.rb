17ecd3fe82d05ee3224e67753e7661ffb9aed8ad
orrbarkat <barkat.orr@gmail.com>
2015-06-27 23:05:19 +0300

    push delete impelment

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -23,6 +23,7 @@ class PublicationsController < ApplicationController
   end
 
   def destroy
+    pushDelete(@publication)
     @publication.destroy
     render json: "OK"
   end
@@ -37,9 +38,9 @@ private
     @publication = Publication.find(params[:id])
   end
 
-  def push(publication)
+def push(publication)
     require 'houston'
-    @devices = ActiveDevice.where(is_ios: true).where.not(remote_notification_token: nil)
+    @devices = ActiveDevice.where(is_ios: true).where.not(remote_notification_token: "no")
     certificate = File.read("/app/lib/assets/ck.pem")#ck_production
     passphrase = "g334613334613fxct"
     connection = Houston::Connection.new(Houston::APPLE_DEVELOPMENT_GATEWAY_URI, certificate, passphrase)
@@ -47,7 +48,7 @@ private
     @devices.each do |device|
       notification = Houston::Notification.new(device: device.remote_notification_token) #'fd01e0baab71ad02ffd4eb10e34daa06fbdb3352ce7286a20ef1333465bc494b'
       notification.alert = "New event around you #{publication.title}" 
-      notification.badge = 2
+      notification.badge = 1
       notification.sound = "default"
       notification.category = "ARRIVED_CATEGORY"
       notification.content_available = false
@@ -56,8 +57,27 @@ private
     end
       connection.close
   end
-
-
+  
+def pushDelete(publication)
+   require 'houston'
+    @devices = ActiveDevice.where(is_ios: true).where.not(remote_notification_token: "no")
+    certificate = File.read("/app/lib/assets/ck.pem")#ck_production
+    passphrase = "g334613334613fxct"
+    connection = Houston::Connection.new(Houston::APPLE_DEVELOPMENT_GATEWAY_URI, certificate, passphrase)
+    connection.open
+    @devices.each do |device|
+      notification = Houston::Notification.new(device: device.remote_notification_token) #'fd01e0baab71ad02ffd4eb10e34daa06fbdb3352ce7286a20ef1333465bc494b'
+      notification.alert = "Event finished around you #{publication.title}" 
+      notification.badge = 1
+      notification.sound = "default"
+      notification.category = "ARRIVED_CATEGORY"
+      notification.content_available = false
+      notification.custom_data = {type:"deleted_publication",data:{ id:publication.id,version:publication.version,title:publication.title}}
+      connection.write(notification.message)
+    end
+      connection.close
+end
+  
   def publication_params
     params.require(:publication).permit(:version, :title, :subtitle, :address, :type_of_collecting, :latitude, :longitude, :starting_date, :ending_date, :contact_info, :is_on_air, :active_device_dev_uuid, :photo_url)
   end
