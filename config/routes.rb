786f9763ce574b92a4adbfa9674942894b9e3aab
orrbarkat <barkat.orr@gmail.com>
2015-10-18 17:01:26 +0300

    gcm path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
 
   #resources :publication_reports
-  get 'publications/gcm' => 'publication#gcm'
+  get 'gcm' => 'publication#gcm'
   post 'publications/new' => 'publication#create'
   resources :active_devices
 
