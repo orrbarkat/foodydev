fa2b301a0eb15a0855c9ebb386e55d29a666637f
orrbarkat <ost2015@gmail.com>
2015-11-09 21:21:18 +0200

    orr's update_2

--- test/controllers/publications_controller_test.rb
+++ test/controllers/publications_controller_test.rb
@@ -5,45 +5,45 @@ class PublicationsControllerTest < ActionController::TestCase
     @publication = publications(:one)
   end
 
-  test "should get index" do
-    get :index
+  test "should get home" do
+    get :home
     assert_response :success
     assert_not_nil assigns(:publications)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
 
   test "should create publication" do
     assert_difference('Publication.count') do
-      post :create, publication: { is_on_air: @publication.is_on_air, publication_address: @publication.publication_address, publication_contact_info: @publication.publication_contact_info, publication_ending_date: @publication.publication_ending_date, publication_latitude: @publication.publication_latitude, publication_longitude: @publication.publication_longitude, publication_starting_date: @publication.publication_starting_date, publication_title: @publication.publication_title, publication_type_of_collecting: @publication.publication_type_of_collecting, publication_version: @publication.publication_version, reporting_device_uuid: @publication.reporting_device_uuid }
+      post :create, publication: { is_on_air: @publication.is_on_air, address: @publication.address, contact_info: @publication.contact_info, ending_date: @publication.ending_date, latitude: @publication.latitude, longitude: @publication.longitude, starting_date: @publication.starting_date, title: @publication.title, type_of_collecting: @publication.type_of_collecting, version: @publication.version, active_device_dev_uuid: @publication.active_device_dev_uuid }
     end
 
-    assert_redirected_to publication_path(assigns(:publication))
+  #   assert_redirected_to publication_path(assigns(:publication))
   end
 
-  test "should show publication" do
-    get :show, id: @publication
-    assert_response :success
-  end
+  # test "should show publication" do
+  #   get :show, id: @publication
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get :edit, id: @publication
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get :edit, id: @publication
+  #   assert_response :success
+  # end
 
-  test "should update publication" do
-    patch :update, id: @publication, publication: { is_on_air: @publication.is_on_air, publication_address: @publication.publication_address, publication_contact_info: @publication.publication_contact_info, publication_ending_date: @publication.publication_ending_date, publication_latitude: @publication.publication_latitude, publication_longitude: @publication.publication_longitude, publication_starting_date: @publication.publication_starting_date, publication_title: @publication.publication_title, publication_type_of_collecting: @publication.publication_type_of_collecting, publication_version: @publication.publication_version, reporting_device_uuid: @publication.reporting_device_uuid }
-    assert_redirected_to publication_path(assigns(:publication))
-  end
+  # test "should update publication" do
+  #   patch :update, id: @publication, publication: { is_on_air: @publication.is_on_air, publication_address: @publication.publication_address, publication_contact_info: @publication.publication_contact_info, publication_ending_date: @publication.publication_ending_date, publication_latitude: @publication.publication_latitude, publication_longitude: @publication.publication_longitude, publication_starting_date: @publication.publication_starting_date, publication_title: @publication.publication_title, publication_type_of_collecting: @publication.publication_type_of_collecting, publication_version: @publication.publication_version, reporting_device_uuid: @publication.reporting_device_uuid }
+  #   assert_redirected_to publication_path(assigns(:publication))
+  # end
 
-  test "should destroy publication" do
-    assert_difference('Publication.count', -1) do
-      delete :destroy, id: @publication
-    end
+  # test "should destroy publication" do
+  #   assert_difference('Publication.count', -1) do
+  #     delete :destroy, id: @publication
+  #   end
 
-    assert_redirected_to publications_path
-  end
+  #   assert_redirected_to publications_path
+  # end
 end
