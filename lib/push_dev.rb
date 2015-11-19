05edf09f52673697237884941bf903a4c540b8be
orrbarkat <barkat.orr@gmail.com>
2015-11-19 13:25:51 +0200

    empty push fix

--- lib/push_dev.rb
+++ lib/push_dev.rb
@@ -30,62 +30,66 @@ def pushDelete(publication)
   #@registered = RegisteredUserForPublication.where("publication_id = ? AND publication_version = ?" , publication.id , publication.version)
   #@devices = @registered.where(is_ios: true).where.not(remote_notification_token: "no")
   registered = iphone_tokens(publication)
-  certificate = File.read(ENV["certificate_path"])#ck_production
-  connection = Houston::Connection.new(Houston::APPLE_DEVELOPMENT_GATEWAY_URI, certificate, ENV["password"])
-  connection.open
-  registered.each do |token|
-    notification = Houston::Notification.new(device: "909cb3d2629c81fd703e35a026d025b1f325e6174b4cb5955aa18dcbe87c3cbf")
-    notification.alert = "Event finished around you #{publication.title}" 
-    notification.badge = 1
-    notification.sound = "default"
-    notification.category = "ARRIVED_CATEGORY"
-    notification.content_available = true
-    notification.custom_data = {type:"deleted_publication",data:{ id:publication.id,version:publication.version,title:publication.title}}
-    connection.write(notification.message)
-    puts "Error: #{notification.error}." if notification.error
-  end        
+  unless registered.empty?
+    certificate = File.read(ENV["certificate_path"])#ck_production
+    connection = Houston::Connection.new(Houston::APPLE_DEVELOPMENT_GATEWAY_URI, certificate, ENV["password"])
+    connection.open
+    registered.each do |token|
+      notification = Houston::Notification.new(device: "909cb3d2629c81fd703e35a026d025b1f325e6174b4cb5955aa18dcbe87c3cbf")
+      notification.alert = "Event finished around you #{publication.title}" 
+      notification.badge = 1
+      notification.sound = "default"
+      notification.category = "ARRIVED_CATEGORY"
+      notification.content_available = true
+      notification.custom_data = {type:"deleted_publication",data:{ id:publication.id,version:publication.version,title:publication.title}}
+      connection.write(notification.message)
+      puts "Error: #{notification.error}." if notification.error
+    end      
   connection.close
+  end
 end
 
 def pushRegistered(publication, registration)
   
   registered = iphone_tokens(publication)
-
-  certificate = File.read(ENV["certificate_path"])#ck_production
-  connection = Houston::Connection.new(Houston::APPLE_DEVELOPMENT_GATEWAY_URI, certificate, ENV["password"])
-  connection.open
-  registered.each do |token|
-    notification = Houston::Notification.new(device: "909cb3d2629c81fd703e35a026d025b1f325e6174b4cb5955aa18dcbe87c3cbf")
-    notification.alert = "User comes to pick up #{publication.title}"
-    notification.badge = 1
-    notification.sound = "default"
-    notification.category = "ARRIVED_CATEGORY"
-    notification.content_available = true
-    notification.custom_data = {type:"registration_for_publication",data:{ id:publication.id,version:publication.version,date:registration.date_of_registration}}
-    connection.write(notification.message)
-    puts "Error: #{notification.error}." if notification.error
-  end   
-  connection.close
+  unless registered.empty?
+    certificate = File.read(ENV["certificate_path"])#ck_production
+    connection = Houston::Connection.new(Houston::APPLE_DEVELOPMENT_GATEWAY_URI, certificate, ENV["password"])
+    connection.open
+    registered.each do |token|
+      notification = Houston::Notification.new(device: "909cb3d2629c81fd703e35a026d025b1f325e6174b4cb5955aa18dcbe87c3cbf")
+      notification.alert = "User comes to pick up #{publication.title}"
+      notification.badge = 1
+      notification.sound = "default"
+      notification.category = "ARRIVED_CATEGORY"
+      notification.content_available = true
+      notification.custom_data = {type:"registration_for_publication",data:{ id:publication.id,version:publication.version,date:registration.date_of_registration}}
+      connection.write(notification.message)
+      puts "Error: #{notification.error}." if notification.error
+    end   
+    connection.close
+  end
 end
 
 def pushReport(publication, report)
   registered = iphone_tokens(publication)
-
-  certificate = File.read(ENV["certificate_path"])#ck_production
-  connection = Houston::Connection.new(Houston::APPLE_DEVELOPMENT_GATEWAY_URI, certificate, ENV["password"])
-  connection.open
-  registered.each do |token|
-    notification = Houston::Notification.new(device: "909cb3d2629c81fd703e35a026d025b1f325e6174b4cb5955aa18dcbe87c3cbf")
-    notification.alert = 'New report'
-    notification.badge = 1
-    notification.sound = "default"
-    notification.category = 'ARRIVED_CATEGORY'
-    notification.content_available = true
-    notification.custom_data = {type:"publication_report",data:{:publication_id=>publication.id,:publication_version=>publication.version,:date_of_report=>report.date_of_report, :report=>report.report}}
-    connection.write(notification.message)
-    puts "Error: #{notification.error}." if notification.error
-  end       
-  connection.close
+  unless registered.empty?
+    certificate = File.read(ENV["certificate_path"])#ck_production
+    connection = Houston::Connection.new(Houston::APPLE_DEVELOPMENT_GATEWAY_URI, certificate, ENV["password"])
+    connection.open
+    registered.each do |token|
+      notification = Houston::Notification.new(device: "909cb3d2629c81fd703e35a026d025b1f325e6174b4cb5955aa18dcbe87c3cbf")
+      notification.alert = 'New report'
+      notification.badge = 1
+      notification.sound = "default"
+      notification.category = 'ARRIVED_CATEGORY'
+      notification.content_available = true
+      notification.custom_data = {type:"publication_report",data:{:publication_id=>publication.id,:publication_version=>publication.version,:date_of_report=>report.date_of_report, :report=>report.report}}
+      connection.write(notification.message)
+      puts "Error: #{notification.error}." if notification.error
+    end       
+    connection.close
+  end
 end
 
 
