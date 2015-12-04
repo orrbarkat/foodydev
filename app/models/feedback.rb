d2f2bd74afe23357761174b949599e0e942513bc
orrbarkat <ost2015@gmail.com>
2015-12-04 13:19:37 +0200

    feedbacks5_fix_models

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,7 +1,7 @@
 class Feedback < ActiveRecord::Base
 	belongs_to :active_device
 
-	validate  :active_device_dev_uuid, presence: true
-	validate  :reporter_name, presence: true, length: {limit:100};
-    validate  :report, presence: true, length: {limit:500};
+	validates  :active_device_dev_uuid, presence: true
+	validates  :reporter_name, presence: true, length: {limit:100};
+    validates  :report, presence: true, length: {limit:500};
 end
--- db/migrate/20151123175031_create_feedbacks.rb
+++ db/migrate/20151123175031_create_feedbacks.rb
@@ -1,11 +1,16 @@
 class CreateFeedbacks < ActiveRecord::Migration
-  def change
+  def up
     create_table :feedbacks do |t|
       t.string :active_device_dev_uuid
       t.string :reporter_name
       t.string :report
 
       t.timestamps null: false
+    end
+    def down
+    	drop_table :feedbacks
+    end
+    	
     end
   end
 end
