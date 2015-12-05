1a4fb847a68e8c1fc98081edfae59136c6eaa397
orrbarkat <barkat.orr@gmail.com>
2015-12-05 02:34:40 +0200

    java syntax in ruby shouldn't work...

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,6 +2,6 @@ class Feedback < ActiveRecord::Base
 	belongs_to :active_device
 
 	validates  :active_device_dev_uuid, presence: true
-    validates  :reporter_name, length: {maximum:100};
-   	validates  :report, presence: true, length: {maximum:500};
+    validates  :reporter_name, length: {maximum:100}
+   	validates  :report, presence: true, length: {maximum:500}
 end
