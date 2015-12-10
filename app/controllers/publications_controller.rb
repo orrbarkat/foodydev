df769e00f76d99c96cb0345c624594a56bf72ce1
orrbarkat <barkat.orr@gmail.com>
2015-12-10 12:37:15 +0200

    time module

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -20,8 +20,10 @@ class PublicationsController < ApplicationController
     require 'houston'
     @publication = Publication.new(publication_params)
     if @publication.starting_date = 0.0
-      @publication.starting_date = Date.civil(params[:publication]["starting_date(1i)"].to_i,params[:publication]["starting_date(2i)"].to_i,params[:publication]["starting_date(3i)"].to_i)
-      @publication.starting_date = Date.civil(params[:publication]["ending_date(1i)"].to_i,params[:publication]["ending_date(2i)"].to_i,params[:publication]["ending_date(3i)"].to_i)
+      @publication.starting_date = Time.new(params[:publication]["starting_date(1i)"].to_i,params[:publication]["starting_date(2i)"].to_i,params[:publication]["starting_date(3i)"].to_i,
+        params[:publication]["starting_date(4i)"].to_i,params[:publication]["starting_date(5i)"].to_i).to_i
+      @publication.ending_date = Time.new(params[:publication]["ending_date(1i)"].to_i,params[:publication]["ending_date(2i)"].to_i,params[:publication]["ending_date(3i)"].to_i,
+        params[:publication]["ending_date(4i)"].to_i,params[:publication]["ending_date(5i)"].to_i).to_i
     end
     puts "one"
     @publication.save!
@@ -30,10 +32,10 @@ class PublicationsController < ApplicationController
     puts "three"
     # pushGcm(@publication)
     puts"four"
-    render json: @publication, only: [:id, :version]
+    render json: @publication#, only: [:id, :version]
     puts "five"
-  # rescue
-  #   render json: @publication.errors, status: :unprocessable_entity
+  rescue
+    render json: @publication.errors, status: :unprocessable_entity
   end
 
   def update
