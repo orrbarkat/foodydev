eadf0acc4fcdd2d4798717950bae4fcbacabebd5
orrbarkat <ost2015@gmail.com>
2015-12-19 23:59:00 +0200

    heroku database

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # config.serve_static_files = true
-  # config.assets.compile = true
-  # config.assets.digest = true
+  config.serve_static_files = true
+  config.assets.compile = true
+  config.assets.digest = true
 end
