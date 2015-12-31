f3c19947d9a8082eee994e83a33332bce377eaa5
orrbarkat <ost2015@gmail.com>
2015-12-31 13:24:17 +0200

    validates test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	  belongs_to :active_device
 
 
-	  validates :identity_provider_token, presence: true
+	  #validates :identity_provider_token, presence: true
       validates :phone_number, presence: true
       validates :identity_provider_email, presence: true
       validates :identity_provider_user_name, presence: true
