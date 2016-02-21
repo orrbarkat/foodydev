b10d522383f700c43b61085f51e9cbc2b8ba2655
gogalefree <gogalefree@gmail.com>
2016-02-22 00:06:38 +0200

    g4

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -44,7 +44,9 @@ class GroupMembersController < ApplicationController
       
         format.html { redirect_to @send_group_members, notice: 'Group members was successfully created.' }
       #  format.json { render :json => @send_group_members , status: :created } 
-      format.json { render :json => @send_group_members } 
+#      format.json { render :json => @send_group_members } 
+            format.json { render @send_group_members } 
+
 
       end
   end
