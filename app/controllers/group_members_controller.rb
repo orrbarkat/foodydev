cfdd602e9c55b3d7b81c4ef5d837b4359d4b9349
gogalefree <gogalefree@gmail.com>
2016-02-22 00:02:06 +0200

    g3

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -30,7 +30,7 @@ class GroupMembersController < ApplicationController
    # members_params = group_member_params
 
     
-    @send_group_members = Array.new
+    @send_group_members = group_member_params
     
   #    members_params.each do |group_member|
        # temp = GroupMember.new(group_member)
