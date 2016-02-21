ab7eb67ce158320d9b6be2194f1d10731c7fda86
orrbarkat <ost2015@gmail.com>
2016-02-21 12:19:00 +0200

    group fix

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
-
+  before_action :group_params, only: [:create]
   # GET /groups
   # GET /groups.json
   def index
@@ -70,6 +70,6 @@ class GroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params[:group]
+     params.require(:group).permit(:user_id, :name)
     end
 end
