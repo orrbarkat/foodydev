4654fc2c51e833612226cc7978f05d6ba1f8329e
orrbarkat <ost2015@gmail.com>
2016-03-02 15:46:22 +0200

    get groups for user-fix9

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
 	belongs_to :user
-	has_many :group_members
+	has_many :group_members, foreign_key: :Group_id
 
 	validates :name, presence: true
 	validates_associated :group_members, :user
