8fe822835d4e1a03548ea7746cd3532eaeb0b139
orrbarkat <barkat.orr@gmail.com>
2016-03-06 13:02:57 +0200

    fixed user#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,8 +68,9 @@ class UsersController < ApplicationController
       
       respond_to do |format|
         if @user.save 
+           
           format.html { redirect_to @user, notice: 'User was successfully created.' }
-          format.json { render :show, status: :created, location: @user }
+          format.json { render :show, status: :created}#, location: @user }
         else
           format.html { render :new }
           format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -81,7 +82,7 @@ class UsersController < ApplicationController
       respond_to do |format|
           if @user.update(user_params)
             format.html { redirect_to @user, notice: 'User was successfully updated.' }
-            format.json { render :show, status: :ok, location: @user }
+            format.json { render :show, status: :ok}#, location: @user }
           else
             format.html { render :edit }
             format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -96,7 +97,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.json { render :show, status: :ok}#, location: @user }
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  pending describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
+	let(:json) { JSON.parse(response.body) }
+  describe "GET /users/:id/groups" do
+    
+    it "is able to get groups" do
+      get get_groups_path(1)
+      puts response.body
       expect(response).to have_http_status(200)
     end
+
+    it "returns an empty array if the user doesnt exist" do
+    	get get_groups_path(100)
+    	expect(json).to eq([])
+    end
+
+    # it "returns all groups I'm admin of" do
+    	# create(:user)
+    	# create(:group, :user_id=>User.last.id)
+    	# create(:group_member, :Group_id => Group.last.id)
+    	# get get_groups_path(User.last.id)
+    	# expect(json).to eq([{Group.last.id, Group.last.name, User.last.id, [GroupMember.last]}])
+    # end
   end
+
+  it "successfuly creates a user" do
+  		 headers = {
+  		 	"ACCEPT" => "application/json",
+  		 	"CONTENT_TYPE" => "application/json"
+  		 }
+  		 user = build(:user).attributes.except(:id,:created_at,:updated_at)
+  		 post "/users", { :user => user }.to_json, headers
+  		expect(response).to have_http_status(200)
+  	end
 end
