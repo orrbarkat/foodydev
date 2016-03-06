2e8c07a1a4167dcbf38aeda32da37be5a7087951
orrbarkat <barkat.orr@gmail.com>
2016-03-06 16:20:12 +0200

    phone fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,17 @@ class User < ActiveRecord::Base
 
 
 	validates :identity_provider_token, presence: true
-      validates :phone_number, presence: true, length: { maximum: 9 }
+      validates :phone_number, presence: true, length: { in: 9...15 }
       validates :identity_provider_email, presence: true
+      validates :identity_provider, inclusion: {in: ["facebook","google"]}
       validates :identity_provider_user_name, presence: true
       validates :active_device_dev_uuid, presence: true
 
-      before_validation :normalize_phone
+      before_save :normalize_phone
 
 
       def normalize_phone
-      	self.phone_number = self.phone_number.gsub(/[^\d]/, '').split(//).last(9).join
+      	self.uniphone = self.phone_number.gsub(/[^\d]/, '').split(//).last(9).join
       end
 
 end
