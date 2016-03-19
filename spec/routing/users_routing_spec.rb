7dfec5534797a569b088fb33446fc46c59432609
orrbarkat <barkat.orr@gmail.com>
2016-03-19 12:28:45 +0200

    travis

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe UsersController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/users").to route_to("users#index")
+      expect(:get => "/users").to route_to("404")
     end
 
     it "routes to #new" do
-      expect(:get => "/users/new").to route_to("users#new")
+      expect(:get => "/users/new").to route_to("404")
     end
 
     it "routes to #show" do
-      expect(:get => "/users/1").to route_to("users#show", :id => "1")
+      expect(:get => "/users/1").to route_to("404")#users#show", :id => "1")
     end
 
     it "routes to #edit" do
