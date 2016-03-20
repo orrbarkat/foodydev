410d54269b726e3ef5b0416fbdcac605105c2f6a
orrbarkat <barkat.orr@gmail.com>
2016-03-20 15:48:54 +0200

    test fix

--- spec/jobs/create_publication_job_spec.rb
+++ spec/jobs/create_publication_job_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe CreatePublicationJob, type: :job do
   it 'enqueues in resque' do
   	pub = create(:publication)
+  	size = Resque.size("default")
   	CreatePublicationJob.perform_now(pub.id)
-  	CreatePublicationJob.perform_later(pub.id)
-  	expect(Resque.size("default")).to eq(1)
+  	expect(Resque.size("default")).to eq(size+1)
   end
 end
--- spec/jobs/delete_publication_job_spec.rb
+++ spec/jobs/delete_publication_job_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe DeletePublicationJob, type: :job do
   it 'enqueues in resque' do
   	pub = create(:publication)
+  	size = Resque.size("default")
   	DeletePublicationJob.perform_now(pub)
-  	DeletePublicationJob.perform_later(pub)
-  	expect(Resque.size("default")).to eq(1)
+  	expect(Resque.size("default")).to eq(size+1)
   end
 end
