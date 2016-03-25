b3365e3b1d9025c11ba8019510e7260b97a00db7
orrbarkat <barkat.orr@gmail.com>
2016-03-25 16:43:41 +0300

    delete job

--- app/jobs/delete_publication_job.rb
+++ app/jobs/delete_publication_job.rb
@@ -4,11 +4,11 @@ class DeletePublicationJob < ActiveJob::Base
 
   def perform(pub_id)
     pub = Publication.find(pub_id)
-    unless pub.nil?
-      Rails.logger.debug "#{self.class.name}: I'm performing my job with arguments: #{pub}"
-      pushDelete(pub)
-      pub.destroy if pub.ending_date==0
-    end
+    Rails.logger.debug "#{self.class.name}: I'm performing my job with arguments: #{pub}"
+    pushDelete(pub)
+    pub.destroy if pub.ending_date==0
+  rescue
+    Rails.logger.warn "Unable to find publication, will ignore: #{e}"
   end
 
 protected
