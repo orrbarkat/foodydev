db153c35931b5e14226307552dde621259b478d0
orrbarkat <barkat.orr@gmail.com>
2016-04-14 10:31:44 +0300

    job spec name
    added add_member notifier
    @ost2015 @gogalefree

--- spec/jobs/add_member_notification_job_spec.rb
+++ spec/jobs/add_member_notification_job_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe AddMembernotificationJob, type: :job do
+RSpec.describe AddMemberNotificationJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
