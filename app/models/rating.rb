0fb608c0c6bcbf945c2f662c211df1e90c40990d
Orr Barkat <barkat.orr@gmail.com>
2016-05-31 20:05:47 +0300

    fix first use

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -10,7 +10,10 @@ class Rating < ActiveRecord::Base
   	def update_user
   		user_id  = Publication.find(publication_id).publisher_id
   		@user = User.find(user_id)
+			@user.ratings=0 if @user.ratings.nil?
+			@user.ratings_count=0 if @user.ratings_count.nil?
 			@user.ratings_count = @user.ratings_count + 1
+			if @user.ratings.nil? || @user.ratings_count.nil?
   		@user.ratings = (@user.ratings*(@user.ratings_count) + self.rate)/(@user.ratings_count)
   		@user.save!
   	end
