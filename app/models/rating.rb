15de9e8fba811ccbf834ffd1c423a312cccfff53
Orr Barkat <barkat.orr@gmail.com>
2016-05-31 20:46:39 +0300

    another trial rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -12,7 +12,7 @@ class Rating < ActiveRecord::Base
   		@user = User.find(user_id)
 			@user.ratings=0 if @user.ratings.nil?
 			@user.ratings_count=0 if @user.ratings_count.nil?
-			@user.ratings_count = @user.ratings_count + 1
+			@user.ratings_count += 1
   		@user.ratings = (@user.ratings*(@user.ratings_count) + self.rate)/(@user.ratings_count)
   		@user.save!
   	end
