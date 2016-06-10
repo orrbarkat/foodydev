823cc11dd66fabddc3cdbf849870dce2ecdd773a
gogalefree <gogalefree@gmail.com>
2016-06-10 08:56:54 +0300

    bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   def get_publications_for_user
    #start adding
    #groups_ids = User.find(params[:id]).group_members.select(:Group_id).map{|member| member.Group_id} #GroupMember.where(:user_id => params[:id]).select(:Group_id)
-   #@publications_to_send = []
+   @publications_to_send = []
     
     user_id = User.find(params[:id])
     group_members = GroupMember.where("user_id = ?" , user_id)
